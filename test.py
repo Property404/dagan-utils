9bbf451cba4333de748af96bc8e12ea936de01ca
Property404 <git@dagans.dev>
2023-09-21 17:36:09 -0500

    Allow use of files in tests

--- test.py
+++ test.py
@@ -2,9 +2,24 @@
 """Run test cases for dagan-utils"""
 
 import json
-import subprocess
-import tempfile
 import os
+import subprocess
+import sys
+import tempfile
+
+
+def run_testcase(case):
+    """Run a single testcase"""
+    expected_stdout = bytes(case["stdout"] + "\n", "ascii")
+    command = case["run"]
+    commands = ["bash", "-c", command]
+    result = subprocess.run(commands, check=True, capture_output=True)
+    if expected_stdout != result.stdout:
+        print("Failed")
+        print(f"\tCommand: {command}")
+        print(f"\tExpected: {expected_stdout}")
+        print(f"\tActual: {result.stdout}")
+        sys.exit(1)
 
 
 def main():
@@ -15,25 +30,34 @@ def main():
 
     cwd = os.getcwd()
     os.environ["PATH"] += f"{cwd}/bin/"
-    tempdir = tempfile.mkdtemp()
-    os.chdir(tempdir)
 
     count = 0
     for case in cases:
-        expected_stdout = bytes(case["output"] + "\n", "ascii")
-        command = case["run"]
-        commands = ["bash", "-c", command]
-        result = subprocess.run(commands, check=True, capture_output=True)
-        if expected_stdout != result.stdout:
-            print("Failed")
-            print(f"\tCommand: {command}")
-            print(f"\tExpected: {expected_stdout}")
-            print(f"\tActual: {result.stdout}")
+        tempdir = tempfile.mkdtemp()
+        os.chdir(tempdir)
+
+        files = []
+        if "files" in case:
+            files = case["files"]
+            for filename in files:
+                with open(filename, "wb") as file:
+                    contents = files[filename]
+                    file.write(bytes(contents.encode("ascii")))
+
+        if "cases" in case:
+            for case in case["cases"]:
+                run_testcase(case)
+        else:
+            run_testcase(case)
+
+        for file in files:
+            os.unlink(file)
+        os.chdir(cwd)
+        os.rmdir(tempdir)
 
         count += 1
-    print(f"Ran {count} tests")
 
-    os.chdir(cwd)
+    print(f"Ran {count} tests")
 
 
 main()
