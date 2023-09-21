f7a1513efbc2878fab46b541358d7cc0c7f2a6c0
Property404 <git@dagans.dev>
2023-09-21 18:03:17 -0500

    Allow exit code to be specified in tests

--- test.py
+++ test.py
@@ -10,15 +10,42 @@ import tempfile
 
 def run_testcase(testcase):
     """Run a single testcase"""
-    expected_stdout = bytes(testcase["stdout"] + "\n", "ascii")
     command = testcase["run"]
     commands = ["bash", "-c", command]
-    result = subprocess.run(commands, check=True, capture_output=True)
-    if expected_stdout != result.stdout:
+
+    try:
+        result = subprocess.run(commands, check=True, capture_output=True)
+    except subprocess.CalledProcessError as exc:
+        result = exc
+    except Exception as exception:
+        raise exception
+
+    if "stdout" in testcase:
+        valid = True
+        expected_stdout = bytes(testcase["stdout"] + "\n", "ascii")
+        if expected_stdout != result.stdout:
+            print("Failed")
+            print(f"\tCommand: {command}")
+            print(f"\tExpected: {expected_stdout}")
+            print(f"\tActual: {result.stdout}")
+            sys.exit(1)
+
+    if "exit" in testcase:
+        valid = True
+        expected_exit_status = int(testcase["exit"])
+        if expected_exit_status != result.returncode:
+            print("Failed")
+            print(f"\tCommand: {command}")
+            print(f"\tExpected status: {expected_exit_status}")
+            print(f"\tActual status: {result.returncode}")
+            sys.exit(1)
+    elif result.returncode != 0:
         print("Failed")
         print(f"\tCommand: {command}")
-        print(f"\tExpected: {expected_stdout}")
-        print(f"\tActual: {result.stdout}")
+        print(f"\tStatus: {result.returncode}")
+
+    if not valid:
+        print("Test case does not test anything!")
         sys.exit(1)
 
 
